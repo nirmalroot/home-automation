f134531b7ab2fcc2ce9b4c9bb663dbf686a10841
Nirmal kishore <62794425+nirmalroot@users.noreply.github.com>
2020-08-01 22:22:41 +0530

    Update config.h

--- config.h
+++ config.h
@@ -2,9 +2,9 @@
 #include <FirebaseESP32.h>
 FirebaseData firebaseData;
 #define FIREBASE_HOST "nirmalroot-0.firebaseio.com"
-#define FIREBASE_AUTH "PngI4xMvrqNIgaHf2orO8AXOtpmKMnurKaRYRYa5"
+#define FIREBASE_AUTH "PngI4xMvrqNIgaHf2or.................."
 #define WIFI_SSID "SUPER-USER"
-#define WIFI_PASSWORD "heymanitsmypassword"
+#define WIFI_PASSWORD "your wifi password"
 int  data1;
 int  fireStatus1;
 int  fireStatus2;
